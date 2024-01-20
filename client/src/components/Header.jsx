8e33d8c75a1059ddcb07a2754591fb1e193a118b
Vraj Shah <vrajsshah34@gmail.com>
2024-01-20 15:17:24 +0530

    completed profile page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa';
 import {Link} from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+
+    const {currentUser} = useSelector((state) => state.user)
+
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-cyan-100 shadow-md'>
 
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to = '/'> 
@@ -14,7 +18,7 @@ export default function Header() {
             </h1>
             </Link>
 
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <form className='bg-white p-3 rounded-lg flex items-center'>
                 <input type='text' placeholder='Search...' 
                 className='bg-transparent focus:outline-none w-24 sm:w-64'
                 />
@@ -28,8 +32,17 @@ export default function Header() {
                 <Link to='/about'>
                 <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer font-semibold'>About</li>
                 </Link>
-                <Link to='/sign-in'>
-                <li className='text-slate-700 hover:underline cursor-pointer font-semibold'>Sign In</li>
+                
+                <Link to='/profile'>
+
+                    {currentUser ? (
+                        <img  className='rounded-full h-7 w-7' src={currentUser.avatar} alt ='profile'/>
+                    ) : (
+                    <li className='text-slate-700 hover:underline cursor-pointer font-semibold'>
+                        Sign In</li>
+                    )
+                }
+                
                 </Link>
             </ul>
 
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Profile() {
+  const {currentUser} = useSelector((state) => state.user);
+
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg  mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'> Profile</h1>
+      <form className='flex flex-col gap-4'>
+          <img src = {currentUser.avatar} alt='profile' className='rounded-full h-24 w-24 object-cover self-center mt-2'/>
+          <input type='text' placeholder='Username' className='border p-3 rounded-lg' id='username'/>
+          <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email'/>
+          <input type='text' placeholder='password' className='border p-3 rounded-lg' id='password'/>
+          <button className='bg-slate-700 text-white rounded-lg p-3 uppercase
+          hover:opacity-90 disabled:opacity-80 mt-7'>Update</button>
+
+      </form>
+
+      <div className='flex justify-between mt-7'>
+        <span className='text-red-600 cursor-pointer font-semibold' >Delete Account</span>
+        <span className='text-red-600 cursor-pointer font-semibold'>Sign Out</span>
+      </div>
+
+    </div>
+
   )
 }
